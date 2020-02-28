da3606923af96c396280529d9d0ea98252f7ad1c
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-28 13:23:31 -0500

    ajout template

--- wp-content/themes/underscores-enfant/front-page.php
+++ wp-content/themes/underscores-enfant/front-page.php
@@ -14,15 +14,37 @@
 
 get_header();
 ?>
+	<h1>Je suis modifié par le front-page .php</h1>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
+			$the_query = new WP_Query( array( 'category_name' => 'devoir' ) );
+			if ( $the_query->have_posts() ) {
+    			while ( $the_query->have_posts() ) {
+       				 $the_query->the_post();
+        				echo get_the_title();
+    				}
+			} 
+			else {
+				echo "no found";
+			}
+			wp_reset_postdata();
+			$query = new WP_Query( array( 'author' => "Elena" ) );
+			if ( $query->have_posts() ) {
+    			while ( $query->have_posts() ) {
+       				 $query->the_post();
+						echo '<p>' . get_the_title() .  '</p>';
+    				}
+			} 
+			else {
+				echo "no found";
+			}
+			wp_reset_postdata();
+			//get_template_part( 'template-parts/content', 'page-acc' );
+			//echo get_the_title();
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
